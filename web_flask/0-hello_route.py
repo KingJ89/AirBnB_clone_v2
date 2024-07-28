241609c3f763c09a03040f78250e3263b6171ab9
KingJ89 <janmutewera@gmail.com>
2024-07-28 11:06:39 -0700

    Task 0 completed successfully

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+#this project task we install flask and we create the first hello world file in flask.
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes+False)
+def hello():
+    return "Hello HBNB!"
+
+if __name__ == "__main__":
+    app.run(tost='0.0.0.0', port=5000)

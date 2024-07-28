7afd1a981fa0039b6f618c441d948ccf1a41699b
KingJ89 <janmutewera@gmail.com>
2024-07-28 11:24:06 -0700

    Task 0 ammended succsessfully

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,9 +6,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/', strict_slashes+False)
+@app.route('/', strict_slashes=False)
 def hello():
     return "Hello HBNB!"
 
 if __name__ == "__main__":
-    app.run(tost='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)

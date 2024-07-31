527ba2fee6dd7696f1faa20bacddc3ebf6b2e507
KingJ89 <janmutewera@gmail.com>
2024-07-31 14:31:40 -0700

    second commit on task 5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Starts a Flask web application with various routes."""
+"""Flask web application with various routes."""
 
 from flask import Flask, render_template
 
@@ -7,36 +7,37 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def home():
-    """Display 'Hello HBNB!'"""
+    """Return 'Hello HBNB!'"""
     return "Hello HBNB!"
 
 @app.route("/hbnb", strict_slashes=False)
 def display_hbnb():
-    """Display 'HBNB'"""
+    """Return 'HBNB'"""
     return "HBNB"
 
 @app.route("/c/<text>", strict_slashes=False)
 def display_c(text):
-    """Display 'C' followed by <text> with underscores replaced by spaces."""
+    """Return 'C' followed by <text> with underscores replaced by spaces."""
     text = text.replace("_", " ")
     return f"C {text}"
 
 @app.route("/python", strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
 def display_python(text="is cool"):
-    """Display 'Python' followed by <text> with underscores replaced by spaces."""
+    """Return 'Python' followed by <text> with underscores replaced by spaces."""
     text = text.replace("_", " ")
     return f"Python {text}"
 
 @app.route("/number/<int:n>", strict_slashes=False)
 def display_number(n):
-    """Display '<n> is a number' if <n> is an integer."""
+    """Return '<n> is a number' if <n> is an integer."""
     return f"{n} is a number"
 
 @app.route("/number_template/<int:n>", strict_slashes=False)
 def display_number_template(n):
-    """Render an HTML page with <n> if <n> is an integer."""
+    """Render HTML page with <n> if <n> is an integer."""
     return render_template("5-number.html", n=n)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
+
